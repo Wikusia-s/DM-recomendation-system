63e331b1504eeb9e2479c50049fb1a0594090541
Wikusia-s <wik.szarzynska@gmail.com>
2024-05-10 10:42:04 +0200

    add functions

--- recommendation.py
+++ recommendation.py
@@ -2,38 +2,6 @@ from scipy.sparse import csr_matrix
 import numpy as np
 import pandas as pd
 
-def create_utility_matrix(df):
-    """
-    Generates a sparse matrix from ratings fataframe.
-
-    Args:
-        df: pandas dataframe containing 3 columns (userId, movieId, rating)
-
-    Returns:
-        utility_matrix: sparse matrix
-        user_mapper: dict that maps user id's to user indices
-        user_inv_mapper: dict that maps user indices to user id's
-        movie_mapper: dict that maps movie id's to movie indices
-        movie_inv_mapper: dict that map movie indices to movie id's
-
-    """
-
-    M = df['userId'].nunique()
-    N = df['movieId'].nunique()
-
-    user_mapper = dict(zip(np.unique(df['userId']), list(range(M))))
-    movie_mapper = dict(zip(np.unique(df['movieId']), list(range(N))))
-
-    user_inv_mapper = dict(zip(list(range(M)), np.unique(df['userId'])))
-    movie_inv_mapper = dict(zip(list(range(N)), np.unique(df['movieId'])))
-
-    user_index = [user_mapper[i] for i in df['userId']]
-    item_index = [movie_mapper[i] for i in df['movieId']]
-
-    utility_matrix = csr_matrix((df['rating'], (user_index, item_index)), shape = (M,N))
-
-    return utility_matrix, user_mapper, movie_mapper, user_inv_mapper, movie_inv_mapper
-
 
 def getTwoDatasets(data):
     """
@@ -56,3 +24,67 @@ def getTwoDatasets(data):
     test_df = pd.DataFrame(data.iloc[split_index:])
 
     return train_df, test_df
+
+def create_mappings(df):
+    """
+    Maps user to movie and movie to user
+
+    Args:
+        df: pandas dataframe
+
+    Returns:
+        user2movie: dictionary that maps user to movie
+        movie2user: dictionary that maps movie to user
+
+    """
+
+
+    user2movie = df.groupby('userId')['movieId'].unique().to_dict()
+    movie2user = df.groupby('movieId')['userId'].unique().to_dict()
+
+    return user2movie, movie2user
+
+
+def create_utility_matrix(df):
+    """
+    Creates a pivot table from the original DataFrame. This results
+    in a new DataFrame where users and movies are organized along rows
+    and columns
+
+    Args:
+        df: pandas dataframe
+
+    Returns:
+        utility_matrix: utility matrix with information about movie ratings
+
+    """
+    utility_matrix = df.pivot_table(index='userId', columns='movieId', values='rating')
+    
+    # Fill NaN values with 0
+    utility_matrix.fillna(0, inplace=True) 
+
+    return utility_matrix
+
+def create_chronological_mapping(df):
+    """
+    Creates movie mapping to make it chronological
+
+    Args:
+        df: pandas dataframe
+
+    Returns:
+        df: pandas dataframe but chronological
+    """
+    mapping_dict = {}
+    for index, value in enumerate(df['movieId'].unique()):
+        mapping_dict[value] = index
+
+    df['movieIndex'] = df['movieId'].map(mapping_dict)
+
+def shrinking_data(n, df):
+    user_likes = df.groupby('userId')['movieId'].count()
+    top_users = user_likes.nlargest(n).index
+
+    top_users_df = df[df['userId'].isin(top_users)].reset_index(drop=True)
+
+    return top_users_df
